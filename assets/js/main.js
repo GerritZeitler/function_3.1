ed0675d8efb66e5244a28b8bf95d6cb4a379a381
supercode <getlucked@gmx.de>
2022-07-19 16:01:05 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+
+function klick1() {
+    document.body.style.background = "rgba(5, 171, 11, 1)"
+    const head1 = document.getElementById("head1")
+    head1.style.color = "rgba(109, 13, 128, 1)"
+}
+
+function klick2() {
+    document.body.style.background = "rgba(97, 94, 99, 1)"
+    const head1 = document.getElementById("head1")
+    head1.style.color = "rgba(201, 169, 37, 1)"
+}
+
+function klick3() {
+    document.body.style.background = "rgba(0, 0, 0, 1)"
+    const head1 = document.getElementById("head1")
+    head1.style.color = "rgba(250, 137, 0, 1)"
+}
+
+function klick4() {
+    document.body.style.background = "rgba(1, 0, 128, 1)"
+    const head1 = document.getElementById("head1")
+    head1.style.color = "rgba(4, 208, 202, 1)"
+}
